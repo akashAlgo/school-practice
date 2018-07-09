3412876e27f1d04d61ef3978d95043b53cafc843
Akash <akash@algorythma.com>
2018-07-09 14:56:52 +0400

    Initial commit

---
+++ src/main/java/com/sample/school/SchoolPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.sample.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolPracticeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sample/school/SchoolPracticeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sample.school;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SchoolPracticeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
